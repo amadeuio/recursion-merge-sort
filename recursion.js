1bbc75bb7a15a830517eba4f7130732f137548dc
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-05-31 10:34:20 -0500

    Add fibs(n) function

--- recursion.js
+++ recursion.js
@@ -1,4 +1,21 @@
 // Warmup: Fibonacci
 
 // Function that returns array containing n Fibonacci sequence items without recursion
-function fibs(n) {}
+function fibs(n) {
+  if (n === 0) {
+    return [];
+  }
+
+  if (n === 1) {
+    return [0];
+  }
+
+  let sequence = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
+  }
+
+  return sequence;
+}
+
