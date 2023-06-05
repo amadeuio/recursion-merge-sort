4a306951cfc4d3c2f012688295d452bb7a41d871
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-06-05 14:05:28 -0500

    Add mergeSort()

--- recursion.js
+++ recursion.js
@@ -36,4 +36,50 @@ function fibsRec(n) {
   return fibs;
 }
 
-console.log(fibsRec(8));
+// merge sort
+
+let array = [4, 8, 6, 2, 1, 7, 5, 3];
+
+// function that takes in an array and returns a sorted array using a
+// recursive merge sort methodology
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  // find the middle point to divide the array into two halves
+  let mid = Math.floor(arr.length / 2);
+
+  // call mergeSort for first half
+  let left = mergeSort(arr.slice(0, mid));
+
+  // call mergeSort for second half
+  let right = mergeSort(arr.slice(mid));
+
+  // merge the two halves sorted
+  return merge(left, right);
+}
+
+// merges two sorted arrays (left and right) into a single sorted array
+function merge(left, right) {
+  let resultArray = [],
+    leftIndex = 0,
+    rightIndex = 0;
+
+  // concatenate values into the resultArray in order
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      resultArray.push(left[leftIndex]);
+      leftIndex++; // move left array cursor
+    } else {
+      resultArray.push(right[rightIndex]);
+      rightIndex++; // move right array cursor
+    }
+  }
+
+  // add remaining element in left or right arrays after concatenating in order
+  return resultArray
+    .concat(left.slice(leftIndex))
+    .concat(right.slice(rightIndex));
+}
+
+mergeSort(array); // 1, 2, 3, 4, 5, 6, 7, 8
